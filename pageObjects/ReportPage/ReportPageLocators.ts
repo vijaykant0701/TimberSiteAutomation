6dcf6f3b1685bf8ece97ba25d318f9b70aa00e6d
vijaymishranintheast <vijay.mishra@nintheast.com>
2025-01-28 01:31:41 +0530

    Added more cases

--- pageObjects/ReportPage/ReportPageLocators.ts
+++ pageObjects/ReportPage/ReportPageLocators.ts
@@ -1,8 +1,9 @@
 
 export const locators = {
-    country :"[placeholder*='Country']",
-    dropdown :".ta-results",
-    emailId :".user__name [type='text']",
+    ReportTab :'li.MuiMenuItem-root:has-text("Reports/Exports")',
+    TimeTracking: 'svg[data-testid="KeyboardArrowDownIcon"]',
+    InOutDropDown: './/*[text()="Custom"]',
+    saveReport :'.//*[text()="Save Report"]',//Save Report
     submit :".action__submit",
     orderConfirmationText :".hero-primary",
     orderId : ".em-spacer-1 .ng-star-inserted"
---
+++ tests/ReportTest.spec.ts
@@ -0,0 +1,13 @@
+import test, { expect } from "../base_methods/fixtures/baseTest"
+import { TimeTrackerPage } from "../pageObjects/TimeTrackerPage/TimeTrackerPage";
+import { testData } from "./testData";
+
+test.describe('TC_003', () => {
+
+  test("Verification of admin downloading time report ", async ({ page, loginPage, dashboardPage, timeTrackerPage, reportPage, adminPage }, testinfo) => {
+    await loginPage.goTo();
+    await loginPage.validLogin(testData.username, testData.password);
+    await reportPage.clickOnReportTab();
+    await reportPage.clickOnDownloads();
+  });
+})
